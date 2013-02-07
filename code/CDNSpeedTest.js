e71d5179b75d95a2879f033c8eb2fe9b5a357aa6
shan.zhou <hyspace@gmail.com>
2013-02-07 15:17:47 +0800

    muti

--- code/CDNSpeedTest.js
+++ code/CDNSpeedTest.js
@@ -3,51 +3,79 @@
     //namespace
     window.CDNSpeedTest = {};
 
-    CDNSpeedTest.Test = function(key,url){
-        this.url = url;
-        this.key = key;
-        this._result = 0;
+    CDNSpeedTest.Test = function(key,url,times){
+        this.url = url || '';
+        this.key = key || '';
+        this.times = times || 1;
+        this._startTime = 0;
+        this._failedTimes = 0;
+        this._result = [];
+        this._progress = -1;
         //if this test is belong to a TestList , this refer to the TestList Obj
         this.ownerList = null;
     }
     CDNSpeedTest.Test.prototype = {
         constructor:CDNSpeedTest.Test,
+        _calc:function(){
+            if(this._failedTimes==this.times)return -1;
+            var i,sum = 0;
+            for(i=0;i<this._result.length;i++){
+                sum += this._result[i];
+            }
+            return Math.round(sum / (this.times - this._failedTimes));
+        },
         result:function(){
+            var _time = this._calc();
             return {
                 key:this.key,
                 url:this.url,
-                time:this._result
+                testTimes:this.times,
+                failedTimes:this._failedTimes,
+                time:_time  
             }
         },
-        run:function(){
-            var obj = this;
-            var startTime;
-            var img = new Image();
-            img.addEventListener('load',function(e){
-                var endTime = (new Date()).getTime();
-                obj._result = endTime - startTime;
+        handleEvent:function(event){
+            var endTime = (new Date()).getTime();
+            switch (event.type) {
+                case 'load':
+                    this._result.push(endTime - this._startTime);
+                    break;
+                case 'error':
+                    this._result.push(-1);
+                    this._failedTimes++;
+                    break;
+            }
+            this._progress ++;
+            if(this._progress < this.times){
+                this._step();
+            }else{
                 var evt = document.createEvent('Events');
                 evt.initEvent('test.finish',false,false);
-                evt.targetObject = obj;
+                evt.targetObject = this;
                 document.dispatchEvent(evt);
-            },false)
-            img.addEventListener('error',function(e){
-                obj._result = -1;
-                var evt = document.createEvent('Events')
-                evt.initEvent('test.error',false,false);
-                evt.targetObject = obj;
-                document.dispatchEvent(evt);
-            },false)
-            startTime = (new Date()).getTime();
-            img.src = this.url + '?t=' + startTime;
+            }
+        },
+        _step:function(){
+            var img = new Image();
+            img.addEventListener('load',this,false);
+            img.addEventListener('error',this,false);
+            this._startTime = (new Date()).getTime();
+            img.src = this.url + '?t=' + this._startTime;
+        },
+        run:function(){
+            if(this._progress!=-1)return;
+            this._progress = 0;
+            this._step();
         },
         reset:function(){
-            this._result = 0;
+            this._progress = -1;
+            this._result = [];
+            this._failedTimes = 0;
         }
     }
     CDNSpeedTest.TestList = function(){
-        this.list = [];
-        this.progress = -1;
+        this._list = [];
+        this._progress = -1;
         for(i=0;i<arguments.length;i++){
             this.add(arguments[i]);
         }
@@ -56,8 +84,8 @@
         constructor:CDNSpeedTest.TestList,
         result:function(){
             var arr = [],i;
-            for(i=0;i<this.list.length;i++){
-                arr.push(this.list[i].result());
+            for(i=0;i<this._list.length;i++){
+                arr.push(this._list[i].result());
             }
             return arr;
         },
@@ -65,8 +93,8 @@
             switch (event.type) {
                 case 'test.error':
                 case 'test.finish':
-                    this.progress ++;
-                    if(this.progress < this.list.length){
+                    this._progress ++;
+                    if(this._progress < this._list.length){
                         this._step();
                     }else{
                         var evt = document.createEvent('Events')
@@ -79,27 +107,27 @@
             }
         },
         _step:function(){
-            this.list[this.progress].run();
+            this._list[this._progress].run();
         },
         add:function(){
             var i;
             for(i=0;i<arguments.length;i++){
-                this.list.push(arguments[i]);
+                this._list.push(arguments[i]);
                 arguments[i].ownerList = this;
             }
         },
         run:function(){
-            if(!this.list.length)return;
-            if(this.progress!=-1)return;
-            this.progress = 0;
+            if(!this._list.length)return;
+            if(this._progress!=-1)return;
+            this._progress = 0;
             document.addEventListener('test.finish', this, false);
             document.addEventListener('test.error', this, false);
             this._step();
         },
         reset:function(){
-            this.progress = -1;
-            for(i=0;i<this.list.length;i++){
-                this.list[i].reset();
+            this._progress = -1;
+            for(i=0;i<this._list.length;i++){
+                this._list[i].reset();
             }
         }
     }
--- readme.md
+++ readme.md
@@ -6,18 +6,18 @@ CDN Speed Test Lib is a js lib for image downloading speed test.
 
 ### test:
 
-    var test = new CDNSpeedTest.Test(key,url);
+    var test = new CDNSpeedTest.Test(key,url,repeatTimes);
 
     document.addEventListener('test.finish',function(e){
         console.log(e.targetObject.result());
     },false)
-    
+
     test.run();
 
 ### test list:
 
-    var test1 = new CDNSpeedTest.Test(key,url);
-    var test2 = new CDNSpeedTest.Test(key,url);
+    var test1 = new CDNSpeedTest.Test(key,url,repeatTimes1);
+    var test2 = new CDNSpeedTest.Test(key,url,repeatTimes2);
 
     var testlist = new CDNSpeedTest.TestList(test1);
     testlist.add(test2);
